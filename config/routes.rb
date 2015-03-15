3b3c7c720c0f8701d5f1c4940d90d3bcfbd0bdd2
Renaat De Vos <renaat@drvn.be>
2015-03-15 11:45:40 +0100

    update

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,9 @@
 Rails.application.routes.draw do
+
+  root to: 'email#new'
+
+  resources :emails
+
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
