906df563c3b54cdd438e3cd4aae841115aea108c
Renaat De Vos <renaat@drvn.be>
2015-03-15 20:44:04 +0100

    sass installed

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,6 @@
 Rails.application.routes.draw do
 
-  root to: 'email#new'
-
+  root to: 'emails#new'
   resources :emails
 
   # The priority is based upon order of creation: first created -> highest priority.
